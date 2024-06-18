052eb0ff3efa154ef4912f9d01e4a2fd5194e622
gitmahin <nimahin25@gmail.com>
2024-06-19 05:28:04 +0600

    added

--- src/utils/cookie.ts
+++
@@ -1,9 +0,0 @@
-import Cookies from 'js-cookie';
-
-export const getCookie = (name: any) => {
-    return Cookies.get(name);
-};
-
-export const setCookie = (name: any, value: any, options: any) => {
-    return Cookies.set(name, value, options);
-};
